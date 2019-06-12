0574144559a12e6f06695020061ddc67f41d84db
JoJOes <jordanxmargi@gmail.com>
2019-06-12 11:30:24 +0200

    Login et inscription

---
+++ Projet_lo07/vue/inscription.php
@@ -0,0 +1,26 @@
+<?php
+require 'fragmentHeader.html';
+?>
+<div class="login-form">
+    <form action="router.php?action=inscription" method="post">
+        <h2 class="text-center">S'inscrire</h2>
+        <div class="form-group">
+            <input type="text" name="login" class="form-control" placeholder="Login">
+        </div>
+        <div class="form-group">
+            <input type="password" name"motDePasse" class="form-control" placeholder="Mot de passe">
+        </div>
+        <div class="form-group">
+            <input type="text" name="nom" class="form-control" placeholder="Nom">
+        </div>
+        <div class="form-group">
+            <input type="text" name"prenom" class="form-control" placeholder="Prénom">
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary btn-block">S'inscrire</button>
+        </div>
+    </form>
+    <p class="text-center"><a href="router.php?action=login">Se connecter</a></p>
+</div>
+<?php
+require 'fragmentFooter.html';
--- Projet_lo07/vue/login.php
+++ Projet_lo07/vue/login.php
@@ -2,14 +2,13 @@
 require 'fragmentHeader.html';
 ?>
 <div class="login-form">
-    <form action="router.php" method="post">
-        <input type="hidden" id="action" value="validerLogin">
-        <h2 class="text-center">Se connecter</h2>       
+    <form action="router.php?action=validerLogin" method="post">
+        <h2 class="text-center">Se connecter</h2>
         <div class="form-group">
-            <input type="text" class="form-control" placeholder="Id">
+            <input type="text" name="login" class="form-control" placeholder="Login">
         </div>
         <div class="form-group">
-            <input type="password" class="form-control" placeholder="Mot de passe">
+            <input type="password" name="motDePasse" class="form-control" placeholder="Mot de passe">
         </div>
         <div class="form-group">
             <button type="submit" class="btn btn-primary btn-block">Se connecter</button>
@@ -17,13 +16,9 @@ require 'fragmentHeader.html';
         <div class="clearfix">
             <label class="pull-left checkbox-inline"><input type="checkbox"> Remember me</label>
             <a href="#" class="pull-right">Oublier le mot de passe?</a>
-        </div>        
+        </div>
     </form>
-    <p class="text-center"><a href="#">Creer un nouveau compte</a></p>
+    <p class="text-center"><a href="router.php?action=inscription">Creer un nouveau compte</a></p>
 </div>
 <?php
 require 'fragmentFooter.html';
-
-
-
-
